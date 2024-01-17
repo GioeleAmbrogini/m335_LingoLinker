05a994f47d507a71f979a8a6791d9518ef3e76bd
GioeleAmbrogini <gioele.ambrogini@edu.tbz.ch>
2024-01-17 19:42:57 +0100

    feat: added model class for translated words

---
+++ app/src/main/java/com/zli/a335_lingolinker/TranslatedWords.java
@@ -0,0 +1,36 @@
+package com.zli.a335_lingolinker;
+
+public class TranslatedWords {
+
+    private String original;
+
+    private String translated;
+
+    private String language;
+
+    public TranslatedWords(String original, String translated, String language) {
+        this.original = original;
+        this.translated = translated;
+        this.language = language;
+    }
+
+    public void setOriginal(String original) {
+        this.original = original;
+    }
+
+    public String getTranslated() {
+        return translated;
+    }
+
+    public void setTranslated(String translated) {
+        this.translated = translated;
+    }
+
+    public String getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(String language) {
+        this.language = language;
+    }
+}
