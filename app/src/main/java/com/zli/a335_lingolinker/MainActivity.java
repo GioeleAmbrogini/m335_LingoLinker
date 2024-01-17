8a5b9b8dfb0492443af65a3f4e38a8ade9d37fd0
GioeleAmbrogini <gioele.ambrogini@edu.tbz.ch>
2024-01-17 16:19:34 +0100

    feat: added new methods

--- app/src/main/java/com/zli/a335_lingolinker/MainActivity.java
+++ app/src/main/java/com/zli/a335_lingolinker/MainActivity.java
@@ -2,13 +2,42 @@ package com.zli.a335_lingolinker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button englishButton;
+
+    private Button frenchButton;
+
+    private Button historyButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initViews();
+        initOnclicks();
+    }
+
+    protected void initViews() {
+        englishButton = findViewById(R.id.englishButton);
+        frenchButton = findViewById(R.id.frenchButton);
+        historyButton = findViewById(R.id.historyButton);
+    }
+
+    protected void initOnclicks() {
+        onClickEnglisch();
+    }
+
+    protected void onClickEnglisch() {
+        englishButton.setOnClickListener(view -> {
+            Intent resultIntent = new Intent(this, EnglishActivity.class);
+            resultIntent.putExtra(EnglishActivity.TRANSLATION_LANGUAGE, "EN");
+            startActivity(resultIntent);
+        });
     }
 }
