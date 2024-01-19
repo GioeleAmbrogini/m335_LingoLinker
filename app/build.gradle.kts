4ac78b32f4aecbc08e9869eb71001b70a7b03b26
GioeleAmbrogini <gioele.ambrogini@edu.tbz.ch>
2024-01-19 15:19:05 +0100

    feat: changed the min version of the api

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
 
     defaultConfig {
         applicationId = "com.zli.a335_lingolinker"
-        minSdk = 31
+        minSdk = 27
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
