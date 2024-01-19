f3105f54f5ebe65a1e30c683f447ef3abee387b1
GioeleAmbrogini <gioele.ambrogini@edu.tbz.ch>
2024-01-19 09:31:07 +0100

    feat: changed min sdk version for better testing

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,7 +8,7 @@ android {
 
     defaultConfig {
         applicationId = "com.zli.a335_lingolinker"
-        minSdk = 34
+        minSdk = 31
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
